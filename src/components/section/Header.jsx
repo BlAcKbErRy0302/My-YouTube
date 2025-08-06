6fbcd52f3dc0e1c7d46b9018ea097a7d6fa4949a
BlAcKbErRy0302 <20030302ljg553@gmail.com>
2025-08-06 14:10:45 +0900

    섹션 컴포넌트 구현하기

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header id='header' role='banner'>Header</header>
+  )
+}
+
+export default header
---
+++ src/components/section/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main id='main'></main>
+  )
+}
+
+export default Main
